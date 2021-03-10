588ee107a50dcf6e1e2b87c2fb1178e6a6af1b0c
Rayhan Mursalin <rmursalin@webmd.net>
2021-03-09 23:32:21 -0500

    Make it work

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ form.addEventListener("submit", e => {
 		}
 
 		//ajax here
-		const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+		const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=imperial`;
 
 		fetch(url)
 			.then(response => response.json())
@@ -56,6 +56,25 @@ form.addEventListener("submit", e => {
 			const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
 				weather[0]["icon"]
 			}.svg`;
+
+			const li = document.createElement("li");
+			li.classList.add("city");
+			const markup = `
+				<h2 class="city-name" data-name="${name},${sys.country}">
+				<span>${name}</span>
+				<sup>${sys.country}</sup>
+				</h2>
+				<div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+				<figure>
+				<img class="city-icon" src="${icon}" alt="${
+				weather[0]["description"]
+			}">
+				<figcaption>${weather[0]["description"]}</figcaption>
+				</figure>
+			`;
+			li.innerHTML = markup;
+			list.appendChild(li);
+			})
 
 			.catch(() => {
 			msg.textContent = "Please search for a valid city 😩";
