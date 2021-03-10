b5de2f852204c3487f672adcb3e0509d713a448d
Rayhan Mursalin <rmursalin@webmd.net>
2021-03-09 23:20:08 -0500

    Add API Key

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-/* SEARCH BY USING A CITY NAME (e.g. athens) OR A COMMA-SEPARATED CITY NAME ALONG WITH THE COUNTRY CODE (e.g. athens,gr)*/
+// Variables
 const form = document.querySelector(".top-banner form");
 const input = document.querySelector(".top-banner input");
 const msg = document.querySelector(".top-banner .msg");
 const list = document.querySelector(".ajax-section .cities");
 
+// Openweather API key
+const apiKey = "dc6e3a6b851e183c0095950485f4c863";
