91c2fabe33e6654690b82265a98d1c4970fa59b2
Rayhan Mursalin <rmursalin@webmd.net>
2021-03-09 23:27:36 -0500

    Filter array

--- script.js
+++ script.js
@@ -14,4 +14,25 @@ form.addEventListener("submit", e => {
 	//check if there's already a city
 	const listItems = list.querySelectorAll(".ajax-section .city");
 	const listItemsArray = Array.from(listItems);
+
+	if (listItemsArray.length > 0) {
+		const filteredArray = listItemsArray.filter(el => {
+		  let content = "";
+		  //athens,gr
+		  if (inputVal.includes(",")) {
+			//athens,grrrrrr->invalid country code, so we keep only the first part of inputVal
+			if (inputVal.split(",")[1].length > 2) {
+			  inputVal = inputVal.split(",")[0];
+			  content = el
+				.querySelector(".city-name span")
+				.textContent.toLowerCase();
+			} else {
+			  content = el.querySelector(".city-name").dataset.name.toLowerCase();
+			}
+		  } else {
+			//athens
+			content = el.querySelector(".city-name span").textContent.toLowerCase();
+		  }
+		  return content == inputVal.toLowerCase();
+		});
   });
