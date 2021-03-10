271c6a43866125f08acfc2378220bd678b5069ba
Rayhan Mursalin <rmursalin@webmd.net>
2021-03-09 23:18:06 -0500

    Add variables

---
+++ script.js
@@ -0,0 +1,6 @@
+/* SEARCH BY USING A CITY NAME (e.g. athens) OR A COMMA-SEPARATED CITY NAME ALONG WITH THE COUNTRY CODE (e.g. athens,gr)*/
+const form = document.querySelector(".top-banner form");
+const input = document.querySelector(".top-banner input");
+const msg = document.querySelector(".top-banner .msg");
+const list = document.querySelector(".ajax-section .cities");
+
