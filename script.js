a23627296b2b29a9500965045c79279dd60e9999
Rayhan Mursalin <rmursalin@webmd.net>
2021-03-09 23:26:06 -0500

    Add event listener

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ const list = document.querySelector(".ajax-section .cities");
 
 // Openweather API key
 const apiKey = "dc6e3a6b851e183c0095950485f4c863";
+
+form.addEventListener("submit", e => {
+	e.preventDefault();
+	let inputVal = input.value;
+  
+	//check if there's already a city
+	const listItems = list.querySelectorAll(".ajax-section .city");
+	const listItemsArray = Array.from(listItems);
+  });
